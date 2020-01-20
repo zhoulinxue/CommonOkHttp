4381ee88204c5ddae2f40bcf67bc3b05ed0375cd
zhouxue@lobochat.cn <zlx871026>
2020-01-20 17:57:09 +0800

    新增api

--- library/src/main/java/org/zhx/common/commonnetwork/HttpManager.java
+++ library/src/main/java/org/zhx/common/commonnetwork/HttpManager.java
@@ -1,5 +1,9 @@
 package org.zhx.common.commonnetwork;
 
+import org.zhx.common.commonnetwork.commonokhttp.CommonOkBuilder;
+
+import retrofit2.Retrofit;
+
 /**
  * Copyright (C), 2015-2020
  * FileName: HttpManager
@@ -9,11 +13,24 @@ package org.zhx.common.commonnetwork;
  */
 public class HttpManager {
     private static HttpManager manager;
+    private CommonOkBuilder builder;
+    private Retrofit.Builder retrofitBuilder;
     public static HttpManager getInstance() {
         if (manager == null)
             synchronized (HttpManager.class) {
                 manager = new HttpManager();
             }
         return manager;
+    }
+
+    public HttpManager() {
+        retrofitBuilder =new Retrofit.Builder();
+    }
+
+    public void init(CommonOkBuilder builder) {
+        this.builder = builder;
+        if (builder != null) {
+
+        }
     }
 }
